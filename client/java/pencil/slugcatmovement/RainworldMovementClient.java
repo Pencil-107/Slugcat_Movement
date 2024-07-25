56678ef483ed0d19166f44adde6b47dff27fcf89
Pencil-107 <136842767+Pencil-107@users.noreply.github.com>
2024-07-25 18:40:52 -0400

    Add files via upload

--- client/java/pencil/slugcatmovement/RainworldMovementClient.java
+++ client/java/pencil/slugcatmovement/RainworldMovementClient.java
@@ -19,13 +19,25 @@ import org.lwjgl.glfw.GLFW;
 
 public class RainworldMovementClient implements ClientModInitializer {
 
+	private static KeyBinding crawlKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+    "key.slugcatmovement.crawl", // The translation key of the keybinding's name
+	InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+	GLFW.GLFW_KEY_LEFT_ALT, // The keycode of the key
+    "category.slugcatmovement.movement" // The translation key of the keybinding's category.
+	));
+
 	@Override
 	public void onInitializeClient() {
 
 		ClientTickEvents.START_CLIENT_TICK.register(client -> {
 			pencil.slugcatmovement.movement.wallJump.tick(client);
 			pencil.slugcatmovement.movement.wallSlide.tick(client);
-			pencil.slugcatmovement.movement.Crawl.tick(client);
+		});
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (crawlKey.isPressed()) {
+				// pencil.slugcatmovement.movement.Crawl.tick(client);
+			}
 		});
 	}
 }
--- client/java/pencil/slugcatmovement/movement/Crawl.java
+++ client/java/pencil/slugcatmovement/movement/Crawl.java
@@ -1,9 +1,12 @@
 package pencil.slugcatmovement.movement;
 
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.EntityPose;
+import net.minecraft.util.math.Box;
 import org.lwjgl.glfw.GLFW;
 import pencil.slugcatmovement.RainworldMovement;
 
@@ -11,11 +14,11 @@ public class Crawl {
     public static float crawlSpeed = 3f;
 
 
-    // Currently Broken
-    public static void tick(MinecraftClient client) {
-        if (client.options.sneakKey.isPressed()) {
-            RainworldMovement.makeSwim(client.player, crawlSpeed);
-            client.player.setMovementSpeed(crawlSpeed);
-        }
-    }
+    // Currently Broken Crawl Mechanic
+    //public static void tick(MinecraftClient client) {
+       // RainworldMovement.makeCrawl(client.player, crawlSpeed);
+       // client.player.setMovementSpeed(crawlSpeed);
+       // client.player.setPose(EntityPose.SWIMMING);
+       // client.player.setSwimming(true);
+    //}
 }
